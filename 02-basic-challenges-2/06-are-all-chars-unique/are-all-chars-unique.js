51a037a52fbd824b4ab62f610a0db0736df22515
milanr21 <milanrawal309@gmail.com>
2024-09-22 20:55:17 +0545

    find missing number

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,43 @@
-function areAllCharactersUnique() {}
+// function areAllCharactersUnique(str) {
+
+//     const charCount = {};
+
+//     for (let i = 0; i < str.length; i++) {
+//         const char = str[i];
+
+//         if (charCount[char]) {
+//             return false;
+//         }
+
+//         charCount[char] = true;
+//         console.log(charCount);
+
+
+//     }
+//     return true;
+
+// }
+
+
+
+function areAllCharactersUnique(str) {
+    const charSet = new Set();
+
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+
+
+        if (charSet.has(char)) {
+            return false; s
+        }
+
+        charSet.add(char);
+    }
+
+    return true;
+
+}
+
+
 
 module.exports = areAllCharactersUnique;
