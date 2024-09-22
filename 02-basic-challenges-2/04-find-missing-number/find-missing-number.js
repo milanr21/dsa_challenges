76cdd14014e2ac9d244c8e635b7784428986fe1a
milanr21 <milanrawal309@gmail.com>
2024-09-22 20:10:22 +0545

    find missing number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,18 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+
+    if (arr.length === 0) return 1;
+
+    const n = arr.length + 1;
+    const realSum = (n * (n + 1)) / 2;
+
+    let currentSum = arr.reduce((sum, val) => sum + val, 0)
+
+    // for (let i = 0; i < arr.length; i++) {
+    //     currentSum += arr[i];
+    // }
+
+    return realSum - currentSum;
+
+}
 
 module.exports = findMissingNumber;
