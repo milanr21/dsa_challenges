caa906abbc2a445a68f478fb99ec8d5d5bcefa0a
milanr21 <milanrawal309@gmail.com>
2024-09-22 17:14:01 +0545

    display likes

--- 02-basic-challenges-2/03-display-likes/display-likes-run.js
+++ 02-basic-challenges-2/03-display-likes/display-likes-run.js
@@ -1,5 +1,5 @@
 const displayLikes = require('./display-likes');
 
-const result = displayLikes(['Alex', 'Jacob', 'Mark', 'Max', 'Jill']);
+const result = displayLikes(['Alex']);
 
 console.log(result);
--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,21 @@
-function displayLikes() {}
+function displayLikes(names) {
+    const length = names.length;
+
+    if (length === 0) {
+        return 'no one likes this';
+    }
+    else if (length === 1) {
+        return `${names[0]} likes this`;
+    }
+    else if (length === 2) {
+        return `${names[0]} and ${names[1]} likes this`;
+    }
+    else if (length === 3) {
+        return `${names[0]}, ${names[1]} and ${names[2]} likes this`;
+    }
+    else {
+        return `${names[0]}, ${names[1]} and ${length - 2} others like this`;
+    }
+}
 
 module.exports = displayLikes;
