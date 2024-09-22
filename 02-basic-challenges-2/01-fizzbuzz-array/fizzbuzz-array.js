0e4c504bec4670db5168df181b251f234256ff69
milanr21 <milanrawal309@gmail.com>
2024-09-22 17:04:12 +0545

    intersection array

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,43 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+    let mainArr = [];
+
+    // for (let i = 1; i <= num; i++) {
+    //     if (i % 3 === 0 && i % 5 === 0) {
+    //         mainArr.push('FizzBuzz');
+    //     }
+    //     else if (i % 3 === 0) {
+    //         mainArr.push('Fizz');
+    //     }
+    //     else if (i % 5 === 0) {
+    //         mainArr.push("Buzz")
+    //     }
+    //     else {
+    //         mainArr.push(i);
+    //     }
+    // }
+    // return mainArr;
+
+
+    for (let i = 1; i <= num; i++) {
+        switch (true) {
+            case i % 3 === 0 && i % 5 === 0:
+                mainArr.push("FizzBuzz");
+                break;
+            case i % 3 === 0:
+                mainArr.push("Fizz");
+                break;
+            case i % 5 === 0:
+                mainArr.push("Buzz");
+                break;
+            default:
+                mainArr.push(i);
+
+        }
+
+    }
+    return mainArr;
+
+
+}
 
 module.exports = fizzBuzzArray;
--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,12 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+
+    let finalArr = [];
+    for (let i = 0; i <= arr1.length; i++) {
+        if (arr2.includes(arr1[i]) && !finalArr.includes(arr1[i])) {
+            finalArr.push(arr1[i]);
+        }
+    }
+    return finalArr;
+}
 
 module.exports = arrayIntersection;
