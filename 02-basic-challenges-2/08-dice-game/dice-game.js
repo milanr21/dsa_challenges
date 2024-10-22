4e0601893d3bb78dbe49e4255ccbfad60831c01d
milanr21 <milanrawal309@gmail.com>
2024-10-22 23:31:42 +0545

    feat: roll the dice

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,31 @@
-function diceGameSimulation() {}
+function rollTheDice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function diceGameSimulation(numOfSimulations) {
+  const results = [];
+
+  for (let i = 0; i < numOfSimulations; i++) {
+    const dice1 = rollTheDice();
+    const dice2 = rollTheDice();
+    const sum = dice1 + dice2;
+    let result = '';
+
+    if (sum === 7 || sum === 11) {
+      result = 'win';
+    } else if (sum === 2 || sum === 3 || sum === 12) {
+      result = 'lose';
+    } else {
+      result = ' roll again';
+    }
+    results.push({
+      dice1,
+      dice2,
+      sum,
+      result,
+    });
+  }
+  return results;
+}
 
 module.exports = diceGameSimulation;
